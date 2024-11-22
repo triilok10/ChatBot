c0cc2032d80c831106d4a7d37531670af4f9ed8d
triilok10 <ittiku3@gmail.com>
2024-11-22 21:16:27 +0530

    Secure Anti Frogery Token Generation Code

--- Program.cs
+++ Program.cs
@@ -1,7 +1,15 @@
+using Microsoft.AspNetCore.Antiforgery;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddAntiforgery(options =>
+{
+    options.HeaderName = "X-XSRF-TOKEN";
+    options.Cookie.Name = ".AspNetCore.Antiforgery";
+});
 
 var app = builder.Build();
 
@@ -9,7 +17,8 @@ var app = builder.Build();
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    // The default HSTS value is 30 days. You may want to change this for production scenarios.
+    // See https://aka.ms/aspnetcore-hsts
     app.UseHsts();
 }
 
@@ -17,6 +26,73 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
+
+app.Use(async (context, next) =>
+{
+    var origin = context.Request.Headers["Host"].FirstOrDefault();
+    if (!string.IsNullOrEmpty(origin) && origin != "localhost:7254")
+    {
+        context.Response.StatusCode = 500;
+        await context.Response.WriteAsync("Unauthorized.");
+        return;
+    }
+
+    // If it's a POST request, check CSRF token
+    if (context.Request.Method == "POST")
+    {
+        var antiforgery = context.RequestServices.GetRequiredService<IAntiforgery>();
+        var tokenFromHeader = context.Request.Headers["X-XSRF-TOKEN"].FirstOrDefault();
+
+        // CSRF validation using the token in the header
+        if (!string.IsNullOrWhiteSpace(tokenFromHeader))
+        {
+            try
+            {
+                await antiforgery.ValidateRequestAsync(context);
+            }
+            catch (Exception)
+            {
+                context.Response.StatusCode = 400;
+                await context.Response.WriteAsync("Invalid CSRF token.");
+                return;
+            }
+        }
+        else
+        {
+            var tokenFromForm = "";
+            try
+            {
+                tokenFromForm = context.Request.Form["__RequestVerificationToken"].FirstOrDefault();
+            }
+            catch (Exception)
+            {
+                context.Response.StatusCode = 500;
+                await context.Response.WriteAsync("Unauthorized.");
+                return;
+            }
+
+            if (string.IsNullOrWhiteSpace(tokenFromForm))
+            {
+                context.Response.StatusCode = 400;
+                await context.Response.WriteAsync("Invalid or missing CSRF token.");
+                return;
+            }
+
+            try
+            {
+                await antiforgery.ValidateRequestAsync(context);
+            }
+            catch (Exception)
+            {
+                context.Response.StatusCode = 400;
+                await context.Response.WriteAsync("Invalid CSRF token.");
+                return;
+            }
+        }
+    }
+
+    await next.Invoke();
+});
 
 app.UseAuthorization();
 
