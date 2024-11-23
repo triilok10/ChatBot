e168af625f23b2eb1837125d146212c54a83550d
triilok10 <ittiku3@gmail.com>
2024-11-23 22:50:36 +0530

    Add the About Us and API Documentation Page

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -23,6 +23,16 @@ namespace ChatBot.Controllers
             return View();
         }
 
+        public IActionResult APIDocumentation()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
